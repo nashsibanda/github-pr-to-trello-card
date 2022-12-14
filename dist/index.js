0ee9e6a2377a8cacb96f5d2dfca2c0c01b033988
Nash Sibanda <nash.sibanda@makeleaps.com>
2022-12-14 15:54:41 +0000

    Add PR commenting

--- dist/index.js
+++ dist/index.js
@@ -13317,15 +13317,25 @@ __nccwpck_require__.r(__webpack_exports__);
 const { context = {} } = _actions_github__WEBPACK_IMPORTED_MODULE_2__;
 const { pull_request } = context.payload;
 
-const trelloCardIdPattern = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-card-id-pattern', { required: false }) || /\/tr-([a-zA-Z0-9]+)\/.*/;
-const trelloApiKey = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-api-key', { required: true });
-const trelloAuthToken = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-auth-token', { required: true });
+const trelloCardIdPattern =
+  _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("trello-card-id-pattern", { required: false }) ||
+  /\/tr-([a-zA-Z0-9]+)\/.*/;
+const trelloApiKey = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("trello-api-key", { required: true });
+const trelloAuthToken = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("trello-auth-token", { required: true });
+const githubToken = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("github-token", { required: true });
+
+const trelloApiAuth = {
+  key: trelloApiKey,
+  token: trelloAuthToken,
+};
 
 function getCardShortcode(branch) {
   console.log(`getCardShortcode(${branch})`);
-  const match = branch.match(trelloCardIdPattern)
+  const match = branch.match(trelloCardIdPattern);
   if (match == null) {
-    throw new Error("PR branch name does not meet the guidelines: must include `tr-[Trello card shortcode]");
+    throw new Error(
+      "PR branch name does not meet the guidelines: must include `tr-[Trello card shortcode]"
+    );
   }
   return match[1];
 }
@@ -13334,15 +13344,58 @@ async function addAttachmentToCard(cardId, link) {
   console.log(`addAttachmentToCard(${cardId}, ${link})`);
   let url = `https://api.trello.com/1/cards/${cardId}/attachments`;
   return await axios__WEBPACK_IMPORTED_MODULE_0__.post(url, {
-    key: trelloApiKey,
-    token: trelloAuthToken, 
-    url: link
-  }).then(response => {
-    return response.status == 200;
-  }).catch(error => {
-    console.error(url, `Error ${error.response.status} ${error.response.statusText}`);
-    return null;
-  });
+      ...trelloApiAuth,
+      url: link,
+    })
+    .then(async (response) => {
+      if (response.status == 200) {
+        return await addCommentToPR(cardId);
+      } else {
+        console.error(
+          url,
+          `Response status: ${response.status}\n\nContents: ${response.data}`
+        );
+      }
+    })
+    .catch((error) => {
+      console.error(
+        url,
+        `Error ${error.response.status} ${error.response.statusText}`
+      );
+      return null;
+    });
+}
+
+async function addCommentToPR(cardId) {
+  console.log(`addCommentToPR(${cardId})`);
+  let url = `https://api.trello.com/1/cards/${cardId}`;
+  const { data: boardData } = await axios__WEBPACK_IMPORTED_MODULE_0__.get(url, trelloApiAuth);
+
+  return await axios__WEBPACK_IMPORTED_MODULE_0__.get(url, trelloApiAuth)
+    .then(async (response) => {
+      if (response.status == 200) {
+        const octokit = _actions_github__WEBPACK_IMPORTED_MODULE_2__.getOctokit(githubToken);
+        const cardData = response.data;
+        const addCommentResp = await octokit.issues.createComment({
+          ...context.repo,
+          issue_number: pull_request.number,
+          body: `Related to **[${cardData.name}](${cardData.shortUrl})** on ${boardData.name}`,
+        });
+        return true;
+      } else {
+        console.error(
+          url,
+          `Response status: ${response.status}\n\nContents: ${response.data}`
+        );
+      }
+    })
+    .catch((error) => {
+      console.error(
+        url,
+        `Error ${error.response.status} ${error.response.statusText}`
+      );
+      return null;
+    });
 }
 
 async function handlePullRequest(data) {
@@ -13357,11 +13410,12 @@ async function handlePullRequest(data) {
 
 async function run() {
   if (pull_request && pull_request.title) {
-    handlePullRequest(pull_request)
+    handlePullRequest(pull_request);
   }
-};
+}
 
-run()
+run();
+
 })();
 
 module.exports = __webpack_exports__;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import * as axios from 'axios';
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as axios from "axios";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 const { context = {} } = github;
 const { pull_request } = context.payload;
 
-const trelloCardIdPattern = core.getInput('trello-card-id-pattern', { required: false }) || /\/tr-([a-zA-Z0-9]+)\/.*/;
-const trelloApiKey = core.getInput('trello-api-key', { required: true });
-const trelloAuthToken = core.getInput('trello-auth-token', { required: true });
+const trelloCardIdPattern =
+  core.getInput("trello-card-id-pattern", { required: false }) ||
+  /\/tr-([a-zA-Z0-9]+)\/.*/;
+const trelloApiKey = core.getInput("trello-api-key", { required: true });
+const trelloAuthToken = core.getInput("trello-auth-token", { required: true });
+const githubToken = core.getInput("github-token", { required: true });
+
+const trelloApiAuth = {
+  key: trelloApiKey,
+  token: trelloAuthToken,
+};
 
 function getCardShortcode(branch) {
   console.log(`getCardShortcode(${branch})`);
-  const match = branch.match(trelloCardIdPattern)
+  const match = branch.match(trelloCardIdPattern);
   if (match == null) {
-    throw new Error("PR branch name does not meet the guidelines: must include `tr-[Trello card shortcode]");
+    throw new Error(
+      "PR branch name does not meet the guidelines: must include `tr-[Trello card shortcode]"
+    );
   }
   return match[1];
 }
@@ -21,16 +31,61 @@ function getCardShortcode(branch) {
 async function addAttachmentToCard(cardId, link) {
   console.log(`addAttachmentToCard(${cardId}, ${link})`);
   let url = `https://api.trello.com/1/cards/${cardId}/attachments`;
-  return await axios.post(url, {
-    key: trelloApiKey,
-    token: trelloAuthToken, 
-    url: link
-  }).then(response => {
-    return response.status == 200;
-  }).catch(error => {
-    console.error(url, `Error ${error.response.status} ${error.response.statusText}`);
-    return null;
-  });
+  return await axios
+    .post(url, {
+      ...trelloApiAuth,
+      url: link,
+    })
+    .then(async (response) => {
+      if (response.status == 200) {
+        return await addCommentToPR(cardId);
+      } else {
+        console.error(
+          url,
+          `Response status: ${response.status}\n\nContents: ${response.data}`
+        );
+      }
+    })
+    .catch((error) => {
+      console.error(
+        url,
+        `Error ${error.response.status} ${error.response.statusText}`
+      );
+      return null;
+    });
+}
+
+async function addCommentToPR(cardId) {
+  console.log(`addCommentToPR(${cardId})`);
+  let url = `https://api.trello.com/1/cards/${cardId}`;
+  const { data: boardData } = await axios.get(url, trelloApiAuth);
+
+  return await axios
+    .get(url, trelloApiAuth)
+    .then(async (response) => {
+      if (response.status == 200) {
+        const octokit = github.getOctokit(githubToken);
+        const cardData = response.data;
+        const addCommentResp = await octokit.issues.createComment({
+          ...context.repo,
+          issue_number: pull_request.number,
+          body: `Related to **[${cardData.name}](${cardData.shortUrl})** on ${boardData.name}`,
+        });
+        return true;
+      } else {
+        console.error(
+          url,
+          `Response status: ${response.status}\n\nContents: ${response.data}`
+        );
+      }
+    })
+    .catch((error) => {
+      console.error(
+        url,
+        `Error ${error.response.status} ${error.response.statusText}`
+      );
+      return null;
+    });
 }
 
 async function handlePullRequest(data) {
@@ -45,8 +100,8 @@ async function handlePullRequest(data) {
 
 async function run() {
   if (pull_request && pull_request.title) {
-    handlePullRequest(pull_request)
+    handlePullRequest(pull_request);
   }
-};
+}
 
-run()
+run();
